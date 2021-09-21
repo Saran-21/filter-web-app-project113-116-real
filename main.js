fe36bf415831fab3d9e1c886a1a0c2c81e83a523
Saran-21 <80886595+Saran-21@users.noreply.github.com>
2021-09-21 15:41:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+noseX = 0;
+noseY = 0;
+
 function preload(){
+    clown_nose = loadImage('https://i.postimg.cc/zv4h3kfb/download-1.png');
 }
 function setup(){
     canvas = createCanvas(300,300);
@@ -13,15 +17,22 @@ function modelLoaded(){
     console.log("posenet is intialized");
 }
 function gotPoses(results){
-    if(results.length <0){
+    if(results.length >0){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
         console.log("nose x = " + results[0].pose.nose.x);
         console.log("nose y = " + results[0].pose.nose.y);
     }
 }
 function draw(){
-image(video,0,0,300,300);
-}
+    image(video,0,0,300,300);
+    //fill(255,0,0);
+    //stroke(255,60,0);
+    //circle(noseX,noseY + 10,20);
+    image(clown_nose,noseX - 10,noseY + 10,130,30);
+    }
+
 function take_snapshot(){
     save("moustachefilter.png")
 }
